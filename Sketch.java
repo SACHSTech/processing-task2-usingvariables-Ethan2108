db08d7628667c7a577970ece6034922144635f1a
Ethan Au <75390842+Ethan2108@users.noreply.github.com>
2022-02-25 02:27:42 +0000

    added code for the drawing

--- Sketch.java
+++ Sketch.java
@@ -23,7 +23,36 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
+
+  // Sun
+    ellipse(460, 50, 150, 150);
+    fill(0, 0, 0);
+
+  // House
+    rect(120, 240, 220, 220);
+    fill(222, 222, 222);
+
+  // Windows
+    rect(130, 290, 50, 50);
+    fill(222, 222, 222);
+
+    rect(200, 290, 50, 50);
+    fill(222, 222, 222);
+
+    rect(270, 290, 50, 50);
+    fill(8, 242, 0);
+
+  // Grass
+    rect(0, 450, 500, 80);
+    fill(216, 227, 148);
+
+  // Pathway
+    rect(200, 450, 70, 80);
+    fill(44, 101, 156);
+
+  // Roof
+    triangle(80, 240, 250, 140, 380, 240);
+    fill(246, 255, 0);
 	
   }
   
