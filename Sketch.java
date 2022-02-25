dd9ef88ba1d7023cded202d2682b4ee435825be1
Ethan Au <75390842+Ethan2108@users.noreply.github.com>
2022-02-25 03:14:57 +0000

    added strokes

--- Sketch.java
+++ Sketch.java
@@ -29,34 +29,42 @@ public class Sketch extends PApplet {
     int scaledHeight = height / 500;
     
     // Sun
+    stroke(0);
     fill(246, 255, 0);
     ellipse(460, 50, 150, 150);
 
     // House
+    stroke(0);
     fill(0, 0, 0);
     rect(120, 240, 220, 220);
     
     // Window 1
+    stroke(0);
     fill(222, 222, 222);
     rect(130, 290, 50, 50);
 
     // Window 2
+    stroke(0);
     fill(222, 222, 222);
     rect(200, 290, 50, 50);
 
     // Window 3
+    stroke(0);
     fill(222, 222, 222);
     rect(270, 290, 50, 50);
 
     // Grass
+    stroke(0);
     fill(8, 242, 0);
     rect(0, 450, 500, 80);
 
     // Pathway
+    stroke(0);
     fill(216, 227, 148);
     rect(200, 450, 70, 80);
 
     // Roof
+    stroke(0);
     fill(44, 101, 156);
     triangle(80, 240, 250, 140, 380, 240);
 	
